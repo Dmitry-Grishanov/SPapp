4dfdebc96c93e822a30520cf05f1efe8a559a8d6
Dmitry-Grishanov <dmitry.grishanov@gmail.com>
2025-02-22 20:03:21 -0600

    test

--- vue-project/src/main.ts
+++ vue-project/src/main.ts
@@ -1,11 +1,20 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+<<<<<<< Updated upstream
+=======
+import { createPinia } from 'pinia'
+
+>>>>>>> Stashed changes
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
+<<<<<<< Updated upstream
+=======
+app.use(createPinia())
+>>>>>>> Stashed changes
 app.use(router)
 
 app.mount('#app')
